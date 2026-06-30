64e166da7da87fabfa47e2125eb6d3a5984f35f8
buenyimj-pixel <buenyimj@gmail.com>
2026-06-30 12:55:38 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -415,5 +415,5 @@ class NigerianAccountBot:
 target_url = "https://nnnrc.com/#/register"
 NUM_ACCOUNTS = 5  # Start with 5 for testing
 
-bot = NigerianAccountBot(start_code=70280)
+bot = NigerianAccountBot(start_code=70285)
 bot.run(target_url, num_accounts=NUM_ACCOUNTS)
