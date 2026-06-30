507435de7cebf4f45682930943c35d0585d0ce77
buenyimj-pixel <buenyimj@gmail.com>
2026-06-30 07:13:24 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-my my from selenium import webdriver
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
