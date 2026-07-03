9f3d76f2577d3c1167afead6bf37b83403ba42fc
buenyimj-pixel <buenyimj@gmail.com>
2026-07-03 17:32:33 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -476,7 +476,7 @@ class NigerianAccountBot:
 # ============================================
 
 target_url = "https://nnnrc.com/#/register"
-NUM_ACCOUNTS = 1
+NUM_ACCOUNTS = 2
 
-bot = NigerianAccountBot(start_code=3068912)
+bot = NigerianAccountBot(start_code=3068917)
 bot.run(target_url, num_accounts=NUM_ACCOUNTS)
